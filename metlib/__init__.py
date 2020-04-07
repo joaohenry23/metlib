5bc01c44a6fd8bc06a95c6a36b285390c52b729c
joaohenry23 <53101118+joaohenry23@users.noreply.github.com>
2020-04-06 23:24:26 -0500

    Add files via upload

---
+++ metlib/__init__.py
@@ -0,0 +1,9 @@
+name = "metlib"
+help = 'help(metlib.functions)'
+help = 'help(metlib.flib)'
+from . import flib
+from .functions import cdiff
+from .functions import hcurl
+from .functions import hdivg
+from .functions import hadv
+__version__ = '0.0.1.0'
