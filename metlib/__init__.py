b70779e9331d5879b1ac35e3f6bd8b715ed4e577
Joao Huamán <53101118+joaohenry23@users.noreply.github.com>
2022-04-17 15:16:00 -0500

    Add files via upload

---
+++ metlib/__init__.py
@@ -0,0 +1,7 @@
+name = "metlib"
+from .functions import *
+__all__ = ['cdiff',
+           'relative_vorticity', 'absolute_vorticity',
+           'divergence', 'advection',
+           'potential_temperature','potential_vorticity']
+__version__ = '0.0.1.3'
